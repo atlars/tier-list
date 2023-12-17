71f8899047fca46d3fdb72bb96c2c2a0579443ab
Lars Thomae <lars.thomae@uni-ulm.de>
2023-12-17 16:26:55 +0100

    Remove custom click-outside directive

--- src/main.ts
+++ src/main.ts
@@ -11,20 +11,4 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
-app.directive('click-outside', {
-  mounted: (el, binding) => {
-    el.clickOutsideEvent = (event: MouseEvent) => {
-      // here I check that click was outside the el and his children
-      if (!(el === event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        binding.value()
-      }
-    }
-    document.addEventListener('click', el.clickOutsideEvent)
-  },
-  unmounted: (el) => {
-    document.removeEventListener('click', el.clickOutsideEvent)
-  }
-})
-
 app.mount('#app')
