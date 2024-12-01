dd37046013363a8222c8e35e23979979a4c4fdaa
Lars Thomae <lars.thomae@aol.com>
2024-12-01 13:58:39 +0100

    ref: remove save button

--- src/components/EditorToolbar.vue
+++ src/components/EditorToolbar.vue
@@ -24,7 +24,7 @@ const emit = defineEmits<{
     </button>
 
     <!-- Save button -->
-    <button
+    <!-- <button
       v-tooltip="'Save'"
       type="button"
       class="rounded-lg bg-blue-600 p-3 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -34,7 +34,7 @@ const emit = defineEmits<{
           d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"
         />
       </svg>
-    </button>
+    </button> -->
 
     <!-- Download button -->
     <button
